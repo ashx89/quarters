fb633624b13255b0823c0c8ae7c8a3566909494a
Ashley Taylor <ashleytaylor14@gmail.com>
2014-02-10 17:04:05 +0000

    updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,22 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Tasks configuration
+        requirejs: {
+
+        }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
    
-     grunt.registerTask('default': []);
+    grunt.registerTask('default': []);
 
 };
