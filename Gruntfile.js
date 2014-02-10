ef2ac727e2322359ecea3aec0fa93395f4a5572d
Ashley Taylor <ashleytaylor14@gmail.com>
2014-02-10 23:22:34 +0000

    Project setups

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,71 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         // Tasks configuration
-        requirejs: {
+        clean: ['dist'],
 
+        requirejs: {
+            js: {
+                options: {
+                    baseUrl:        'public/js',
+                    name:           'main',
+                    optimize:       'uglify',
+                    paths:          {'req': 'libs/require'},
+                    mainConfigFile: 'public/js/main.js',
+                    include:        ['req'],
+                    out:            'public/js/app.min.js'
+                }
+            },
+            css: {
+                options: {
+                    cssIn:          'public/css/global.css',
+                    out:            'dist/css/global.css'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: 4000,
+                nospawn: true
+            },
+            html: {
+                files: ['views/**/*.ejs']
+            },
+            css: { 
+                files: ['public/css/*.css']
+            },
+            js: {
+                files: ['public/js/**/*.js']
+            },
+            scss: {
+                files: ['public/scss/**/*.scss'],
+                tasks: ['compass']
+            }
+        },
+
+        compass: {
+            public: {
+                options: {
+                    sassDir:        'public/scss',
+                    cssDir:         'public/css',
+                    fontsDir:       'public/fonts',
+                    imagesDir:      'public/images',
+                    javascriptsDir: 'public/js',
+                    outputStyle:    'compressed',
+                    force: true
+                }
+            },
+            prod: {
+                options: {
+                    sassDir:        'dist/scss',
+                    cssDir:         'dist/css',
+                    fontsDir:       'dist/fonts',
+                    imagesDir:      'dist/images',
+                    javascriptsDir: 'dist/js',
+                    outputStyle:    'compressed',
+                    force: true
+                }
+            }
         }
 
     });
@@ -17,6 +80,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
    
-    grunt.registerTask('default': []);
+    grunt.registerTask('build', [
+        'clean',
+        'requirejs',
+        'watch'
+    ]);
 
 };
